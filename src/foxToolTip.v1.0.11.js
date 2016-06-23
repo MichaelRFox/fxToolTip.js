14691c67dcff679f35d2be39f603ab2f1ffa2b15
MichaelRFox <MichaelRFox@users.noreply.github.com>
2016-06-23 15:13:53 -0400

    latest changes

--- src/foxToolTip.v1.0.11.js
+++ src/foxToolTip.v1.0.11.js
@@ -1,12 +1,20 @@
-/* The MIT License (MIT)
-
-Copyright (c) 2016 Michael R. Fox
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/** 
+* @file foxToolTip.js
+* @version 1.0.11
+* @author Michael R Fox
+* @copyright (c) 2016 Michael R. Fox
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 
+* Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+* distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+* following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 (function () {
 	var tips = [];
@@ -115,17 +123,23 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 	};
 
 	function getElementCoordinates (element) {
+		var cursorBuffer = 15;
 		var clientRect = {};
 
 		var target = tips[tipsIndex.indexOf(element.id)];
 		
 		if (target.mousePoint() == true) {
-			clientRect.left = mouseX - 20;
-			clientRect.top = mouseY - 20;
-			clientRect.right = mouseX + 20;
-			clientRect.bottom = mouseY + 20;
-		} else clientRect = element.getBoundingClientRect();
-
+			clientRect.left = mouseX - cursorBuffer;
+			clientRect.top = mouseY - cursorBuffer;
+			clientRect.right = mouseX + cursorBuffer;
+			clientRect.bottom = mouseY + cursorBuffer;
+		} else {
+			clientRect = element.getBoundingClientRect();
+			clientRect.left -= cursorBuffer;
+			clientRect.top -= cursorBuffer;
+			clientRect.right += cursorBuffer;
+			clientRect.bottom += cursorBuffer;
+		};
 		var height = clientRect.bottom - clientRect.top;
 		var width = clientRect.right - clientRect.left;
 
